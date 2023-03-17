3db5d7a3f7258813f1aec27a1a13c1b9b3670b21
wujh <JunCode@aliyun.com>
2023-03-17 14:07:34 +0800

    fix: webpack配置-babel配置, 并增加相关的注释

--- config/config.js
+++ config/config.js
@@ -15,17 +15,22 @@ module.exports = {
         extensions: ['.js', '.ts', '.tsx', '.css', '.json'],
     },
     module: {
-        // loader是加载器
+        // loader是加载器，在webpack处理某些模块之前，先使用loader进行处理
         rules: [
             {
-                test: /\.tsx?$/,
+                // 当webpack工作时,遇到了以.tsx结尾给模块, 先使用babel-loader进行加载
+                // 当loader加载完后, 还需要使用一些@babel/* 进行语法转化, 转化成ES5代码
+                test: /\.(js|ts|tsx)?$/,
                 use: {
                     loader: 'babel-loader',
+                    // @babel/preset-env是一个预设(预设是插件的集合), 它可以把ES6中的大部分语法, 转化成ES5; 它并不是转化所有语法, 仅仅是大部分, 个别语法转化不了, 需要单独安装插件.
+                    // 如果要使用@babel/preset-env, 需要依赖@babel/core, 它是核心包
+                    // .jsx文件需要@babel/preset-react进行预设处理
                     options: { presets: ['@babel/preset-react', '@babel/preset-env'] },
                 },
             },
             {
-                test: /\.tsx?$/,
+                test: /\.(ts|tsx)?$/,
                 use: 'ts-loader', // 解析TS文件
                 exclude: /node_modules/,
             },
